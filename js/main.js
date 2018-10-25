c09dec36b9e5baaf496483f9ee0720169b20e0fd
lyj1018 <1942962158@qq.com>
2018-10-25 12:50:36 +0800

    project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//配置路径
+require.config({
+	'paths' : {
+		'jquery' : "jquery-1.11.3",
+		'cookie' : "jquery.cookie"
+	}
+})
+
+//引入依赖
+require(['jquery','cookie'],function($,cookie){
+	//操作页面的代码
+	$(function(){
+		alert('hehe');
+	})
+})
